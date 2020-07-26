633a217a4596e420105a64dff8e49c252b3f3c7e
Ignacio Cascos <62115195+icascos@users.noreply.github.com>
2020-07-26 19:00:02 +0200

    Add files via upload

---
+++ example_univariate.R
@@ -0,0 +1,37 @@
+# several interval depths w.r.t. a sample of 100 standard normal observations
+
+par(mar=c(2.5,0,1,0))
+plot(NA,xlim=c(-6,6),ylim=c(0,1),axes=F,ann=F)
+axis(1, -5:5)
+
+set.seed(123)
+gaussian1 <- rnorm(100,0,1)
+gaus1 <- cbind(gaussian1, rep(0.18, 100))
+segments(-3.8, 0.18,  3.8, 0.18, col=176)
+points(gaus1, pch=16, cex=0.56)
+legend(3, 0.28, "N(0,1)", bty="n", cex=0.75)
+zon<-m1m2(gaussian1)
+segments(zon[1],0.16,zon[1],0.2,col=2)
+segments(zon[2],0.16,zon[2],0.2,col=2)
+legend(-6.5,0.28,paste("depth=",round(sm1m2depth(gaussian1,gaussian1),2),sep=""),bty="n",cex=0.8)
+
+mu <- c(0,0,0,1)
+sigma <- c(1,0.5,2,1)
+height <- c(.36,.54,.72,.9)
+
+for(i in 1:4) {
+  gaussian2 <- rnorm(10,mu[i],sigma[i])
+  gaus2 <- cbind(gaussian2, rep(height[i], 10))
+  segments(-3.8, height[i], 3.8, height[i], col=176)
+  gaus12 <- cbind(gaussian1, rep(height[i], 100))
+  points(gaus12, pch=16, cex=0.56, col="honeydew4")
+  points(gaus2, pch=17, cex=0.95, col=1)
+  zon<-m1m2(gaussian2)
+  segments(zon[1],height[i]-0.02,zon[1],height[i]+0.02,col=2)
+  segments(zon[2],height[i]-0.02,zon[2],height[i]+0.02,col=2)
+  legend(3, height[i]+.1,paste("N(",mu[i],",",sigma[i],")",sep=""), bty="n", cex=0.75)
+  legend(-6.5,height[i]+.1,paste("depth=",round(sm1m2depth(gaussian2,gaussian1),2),sep=""),bty="n",cex=0.8)
+}
+
+title(main="Interval depths")
+
---
+++ interval_depth.R
@@ -0,0 +1,168 @@
+# zdepth(x,s) computes the univariate zonoid depth of x w.r.t. sample s
+# can handle any discrete distribution with probability vector prob
+
+zdepth <- function(x,s,prob=rep(1/length(s),length(s)),ord=FALSE) {
+  prob=prob/sum(prob)
+  if(ord==FALSE) {or<-order(s);prob<-prob[or];s<-s[or]}
+  n <- length(s)
+  if((x<s[1])|(x>s[n])) {return(0)}
+  me <- sum(s*prob)
+  if(x==sum(s*prob)) {return(1)}
+  if(x>me) {x <- -x ; s <- -s[n:1] ; prob <- prob[n:1]}
+  i <- 1
+  ac <- s[i]*prob[i]
+  ap <- prob[i]
+  while(x*ap>=ac) {i <- i+1 ; ap <- ap+prob[i] ; ac <- ac+s[i]*prob[i]}
+  return((ap*s[i]-ac)/(s[i]-x))
+}
+
+
+# m1m2obj is the objective function to maximize for the interval depth
+
+m1m2obj<-function(m,x,lb){
+  n <- length(x)
+  if(lb==0 | lb==1) return(0)
+  r <- floor(n*lb)+1
+  z1 <- zdepth(m[1],x,prob=c(rep(1/(n*lb),r-1),(n*lb-floor(n*lb))/(n*lb),rep(0,n-r)),ord=TRUE)
+  z2 <- zdepth(m[2],x,prob=c(rep(0,r-1),(r-n*lb)/(n*(1-lb)),rep(1/(n*(1-lb)),n-r)),ord=TRUE)
+  return(2*min(lb*z1,(1-lb)*z2))
+}
+
+
+# m1m2depth(m,x) computes the interval depth of m w.r.t. sample x
+
+m1m2depth<-function(m,x,ord=FALSE) {
+  if(m[2]<m[1]) return(0)
+  if(ord==FALSE) x <- sort(x)
+  lo <- mean(x<m[1])
+  up <- mean(x<=m[2])
+  if(lo==1 | up==0 | up==lo) {return(0)}
+  return(optimize(m1m2obj,m=m,x=x,lower=lo,upper=up,maximum=TRUE)$objective)
+}
+
+
+# m1m2(samp) computes the zonoid interval of level 1/2 of sample samp
+
+m1m2<-function(samp) {
+  mu<-mean(samp)
+  desv<-mean(abs(samp-median(samp)))
+  return(c(mu-desv,mu+desv))
+}
+
+# sm1m2depth(samp,x) computes the interval depth of the zonoid interval 
+# of level 1/2 of sample samp w.r.t. sample x
+
+sm1m2depth<-function(samp,x,ord=FALSE) {
+  return(m1m2depth(m=m1m2(samp),x,ord))
+}
+
+
+# objective function of 2-dimensional region depth
+
+m1m2r2obj<- function(ang,samp,X) {
+  return(sm1m2depth(samp=cos(ang)*samp[,1]+sin(ang)*samp[,2],
+                   x=cos(ang)*X[,1]+sin(ang)*X[,2]))
+}
+
+m1m2r2objold<- function(ang,samp,X) {
+  x <- cos(ang)*X[,1]+sin(ang)*X[,2]
+  samp <- cos(ang)*samp[,1]+sin(ang)*samp[,2]
+  mu <- mean(samp)
+  desv <- mean(abs(samp-median(samp)))
+  m <- c(mu-desv,mu+desv)
+  return(m1m2depth(m,x))
+}
+
+# 2-dimensional region depth of zonoid region of level 1/2 of samp wrt X
+
+m1m2depthr2<-function(samp,X,nang=10) {
+  depth <- NULL
+  for(i in 1:nang) {
+    depth<- min(depth,
+                optimize(m1m2r2obj,samp=samp,X=X,lower=2*pi*(i-1)/nang,upper=2*pi*i/nang,maximum=FALSE,tol=.Machine$double.eps^0.25)$objective)
+  }
+  return(depth)
+}
+
+# objective function of 3-dimensional region depth
+
+m1m2r3obj <- function(ang,samp,X) {
+  return(sm1m2depth(samp=sin(ang[1])*cos(ang[2])*samp[,1]+sin(ang[1])*sin(ang[2])*samp[,2]+cos(ang[1])*samp[,3],
+                   x=sin(ang[1])*cos(ang[2])*X[,1]+sin(ang[1])*sin(ang[2])*X[,2]+cos(ang[1])*X[,3]))
+}
+
+m1m2r3objold <- function(ang,samp,X) {
+  x <- sin(ang[1])*cos(ang[2])*X[,1]+sin(ang[1])*sin(ang[2])*X[,2]+cos(ang[1])*X[,3]
+  samp <- sin(ang[1])*cos(ang[2])*samp[,1]+sin(ang[1])*sin(ang[2])*samp[,2]+cos(ang[1])*samp[,3]
+  mu <- mean(samp)
+  desv <- mean(abs(samp-median(samp)))
+  m <- c(mu-desv,mu+desv)
+  return(m1m2depth(m,x))
+}
+
+# 3-dimensional region depth of zonoid region of level 1/2 of samp wrt X
+
+m1m2depthr3<-function(samp,X,nang=10) {
+  depth <- 1
+  for(th in pi*seq(from=0,to=1,length.out=nang)[-1]) {
+    for(phi in 2*pi*seq(from=0,to=1,length.out=nang)[-1]) {
+      depth <- min(depth,optim(c(th,phi),m1m2r3obj,samp=samp,X=X)$value)
+    }
+  }
+  return(depth)
+}
+
+# extreme points of the zonoid region of level 1/2 of the bivariate dataset data
+
+zonoidreg <- function(data,nlines=1000){
+  n <- nrow(data)
+  angle <- seq(0,2*pi,length.out=nlines) 
+  extreme.points <- NULL
+  zon1 <- mean(sort(data[,1]*cos(angle[1])+data[,2]*sin(angle[1]))[(n/2+1):n])
+  
+  for(i in 2:nlines){
+    zon2 <-  mean(sort(data[,1]*cos(angle[i])+data[,2]*sin(angle[i]))[(n/2+1):n])
+    extreme.points <- rbind(extreme.points,solve(matrix(c(cos(angle[i-1]),sin(angle[i-1]),
+                                                          cos(angle[i]),sin(angle[i])),byrow=TRUE,ncol=2),
+                                                 c(zon1,zon2)))
+    zon1 <- zon2
+  }
+  extreme.points <- extreme.points[chull(extreme.points),]
+  extreme.points <- rbind(extreme.points,extreme.points[1,])
+  return(extreme.points)
+}
+
+
+
+
+
+
+# some examples
+
+x.data=rnorm(1000)
+mu=mean(x.data)
+desv=mean(abs(x.data-median(x.data)))
+m1m2depth(c(mu-desv,mu+desv),x.data)
+
+
+x1=rnorm(1000)
+x2=rnorm(1000)
+x.data=cbind(x1,x2)
+m1m2depthr2(x.data,x.data)
+
+######################################3
+
+x=seq(-3,3,by=.025)
+y=seq(-3,3,by=.025)
+dat=sort(rnorm(100))
+dep=matrix(nrow=length(x),ncol=length(y))
+for(i in 1:length(x)){
+  for(j in 1:length(y)){
+    dep[i,j]=m1m2depth(c(x[i],y[j]),dat,ord=TRUE)
+  }
+}
+
+
+plot(NULL,xlim=c(-3,3),ylim=c(-3,3))
+contour(x,y,dep,levels=(1:10)/10,add=TRUE)
+filled.contour(x,y,dep)
